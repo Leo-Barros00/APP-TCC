9ea2410632c77e2479006e784325ce4b494285bb
Filipe Neves <filipe.nevser@gmail.com>
2023-04-16 01:09:44 -0300

    feat: add axios

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,7 @@ module.exports = function(api) {
           "@Hooks": "./src/hooks",
           "@Store": "./src/store",
           "@Utils": "./src/utils",
+          "@Api": "./src/api",
         }
       }],
     ]
