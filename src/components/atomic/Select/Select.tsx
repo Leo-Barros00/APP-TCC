19f606514990036ed02276a22a4d33582256d828
Filipe Neves <filipe.nevser@gmail.com>
2023-04-16 20:38:57 -0300

    feat: add select component

---
+++ src/components/atomic/Select/Select.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { View } from 'react-native'
+
+import TextButton from '@Components/atomic/TextButton'
+import Modal from '@Components/atomic/Modal'
+
+import { ISelect } from './interface'
+
+const Select: React.FC<ISelect> = ({
+  title,
+  options,
+  selectedOption,
+  onSelect,
+  disabled,
+}) => {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  function handleOnPressSelectButton() {
+    setIsModalVisible(true)
+  }
+
+  function handleOnPressOptionButton(optionId: string) {
+    onSelect(optionId)
+    setIsModalVisible(false)
+  }
+
+  return (
+    <View>
+      <TextButton
+        text={selectedOption ? selectedOption.value : title}
+        variant="primary"
+        onPress={handleOnPressSelectButton}
+        disabled={disabled}
+      />
+      <Modal
+        isModalVisible={isModalVisible}
+        setIsModalVisible={setIsModalVisible}
+        title={title}
+      >
+        {options?.map(({ id, value }) => (
+          <TextButton
+            key={id}
+            variant="primary"
+            text={value}
+            onPress={() => handleOnPressOptionButton(id)}
+          />
+        ))}
+      </Modal>
+    </View>
+  )
+}
+
+export default Select
