69d3d9285c7a26a2aa29fc6cedd947565b056012
Filipe Zanelato <filipe_zanelato@hotmail.com>
2023-05-13 17:29:49 -0300

    Add error message to Login page

--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,27 +1,28 @@
-import UserService from '@Api/services/userService';
-import EmailTextField from '@Components/atomic/EmailTextField/EmailTextField';
-import PasswordTextField from '@Components/atomic/PasswordTextField/PasswordTextField';
-import TextButton from '@Components/atomic/TextButton/TextButton';
-import { useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
+import UserService from '@Api/services/userService'
+import EmailTextField from '@Components/atomic/EmailTextField/EmailTextField'
+import PasswordTextField from '@Components/atomic/PasswordTextField/PasswordTextField'
+import TextButton from '@Components/atomic/TextButton/TextButton'
+import SignUpErrors from '@Components/signUp/SignUpErrors/SignUpErrors'
+import { useNavigation } from '@react-navigation/native'
+import { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
 
 const Container = styled.View`
-flex: 1;
-width: 100%;
-align-items: center;
-padding:  16px;
+  flex: 1;
+  width: 100%;
+  align-items: center;
+  padding: 16px;
 `
 
 const TitleContainer = styled.View`
-width: 100%;
-align-items: center;
+  width: 100%;
+  align-items: center;
 `
 
 const SignUpTitle = styled.Text`
-font-size: 34px;
-font-family: 'Poppins-Bold';
+  font-size: 34px;
+  font-family: 'Poppins-Bold';
 `
 
 const Instruction = styled.Text`
@@ -36,50 +37,59 @@ const InputContainer = styled.View`
   width: 100%;
 `
 const SignIn = () => {
-    const [ email, setEmail ] = useState('');
-    const [ password, setPassword ] = useState('');
-    const navigation = useNavigation()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState<string[]>([])
+  const navigation = useNavigation()
 
   async function handleLoginPressButton() {
     const loginResponse = await UserService.signIn(email, password)
-
-        if(loginResponse) {
-            navigation.navigate('Main');
-        }
+    console.log(JSON.stringify(loginResponse))
+    if (loginResponse.status !== 'error') {
+      setErrors([]);
+      navigation.navigate('Main')
+    } else {
+      setErrors(['Dados insieridos estão inválidos! Tente novamente'])
     }
+  }
 
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-        <Container>
-            <TitleContainer>
-                <SignUpTitle>Login: </SignUpTitle>
-                <Instruction>Insira seus dados corretamente</Instruction>
-            </TitleContainer>
-            <InputContainer>
-                <EmailTextField
-                    variant="primary"
-                    fluid
-                    onChangeText={setEmail}
-                    placeholder="E-mail"
-                    value={email}
-                    blurOnSubmit={false}
-                    returnKeyType="next"
-                />
-                <PasswordTextField
-                    variant="primary"
-                    fluid
-                    onChangeText={setPassword}
-                    value={password}
-                    placeholder="Senha"
-                    blurOnSubmit={false}
-                    returnKeyType="next"
-                />
-                <TextButton text={'Entrar'} variant={'primary'} fluid onPress={handleLoginPressButton}/>
-            </InputContainer>
-        </Container>
-      </SafeAreaView>
-
-    )
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Container>
+        <TitleContainer>
+          <SignUpTitle>Login: </SignUpTitle>
+          <Instruction>Insira seus dados corretamente</Instruction>
+        </TitleContainer>
+        <InputContainer>
+          <SignUpErrors errors={errors} />
+          <EmailTextField
+            variant="primary"
+            fluid
+            onChangeText={setEmail}
+            placeholder="E-mail"
+            value={email}
+            blurOnSubmit={false}
+            returnKeyType="next"
+          />
+          <PasswordTextField
+            variant="primary"
+            fluid
+            onChangeText={setPassword}
+            value={password}
+            placeholder="Senha"
+            blurOnSubmit={false}
+            returnKeyType="next"
+          />
+          <TextButton
+            text={'Entrar'}
+            variant={'primary'}
+            fluid
+            onPress={handleLoginPressButton}
+          />
+        </InputContainer>
+      </Container>
+    </SafeAreaView>
+  )
 }
 
 export default SignIn
