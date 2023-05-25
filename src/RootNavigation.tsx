2b98e4693efe071dddc0e7ba86b5e995f4786af8
Filipe Neves <filipe.nevser@gmail.com>
2023-05-24 22:56:58 -0300

    fix: navigation login to change between authenticated and unauthenticated

--- src/RootNavigation.tsx
+++ src/RootNavigation.tsx
@@ -2,95 +2,65 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { MaterialIcons } from '@expo/vector-icons'
-import Onboarding from './screens/Onboarding'
-import SignUp from './screens/SignUp'
+
+import NavBar from '@Components/atomic/NavBar/NavBar'
+import Onboarding from '@Screens/Onboarding'
+import SignUp from '@Screens/SignUp'
 import Home from '@Screens/Home/Home'
 import Profile from '@Screens/Profile/Profile'
 import SignIn from '@Screens/SignIn/SignIn'
 import SearchServices from '@Screens/SearchServices/SearchServices'
 import AddHouse from '@Screens/AddHouse/AddHouse'
-import ProposalsStackScreen from '@Screens/Proposals'
-import { Entypo } from '@expo/vector-icons'
+import ProposalsScreen from '@Screens/Proposals'
+import { useAppSelector } from '@Hooks/redux'
+import Preferences from '@Screens/Preferences/Preferences'
 
 const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator()
 
 const RootNavigation = () => {
+  const { isLogged } = useAppSelector(({ auth }) => auth)
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Onboarding"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Onboarding" component={Onboarding} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="Main" component={NavBarNavigation} />
-        <Stack.Screen name="AddHouse" component={AddHouse} />
-      </Stack.Navigator>
+      {isLogged ? <AuthenticatedNavigation /> : <UnauthenticatedNavigation />}
     </NavigationContainer>
   )
 }
 
-const NavBarNavigation = () => {
+const UnauthenticatedNavigation = () => {
+  return (
+    <Stack.Navigator initialRouteName="Onboarding" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Onboarding" component={Onboarding} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="SignIn" component={SignIn} />
+    </Stack.Navigator>
+  )
+}
+
+const AuthenticatedNavigation = () => {
+  return (
+    <Stack.Navigator initialRouteName="Main" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Main" component={MainNavigation} />
+      <Stack.Screen name="AddHouse" component={AddHouse} />
+      <Stack.Screen name="Preferences" component={Preferences} />
+    </Stack.Navigator>
+  )
+}
+
+const MainNavigation = () => {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName
-
-          if (route.name === 'Home') {
-            iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline'
-
-            return <MaterialIcons name={'home'} size={size} color={color} />
-          } else if (route.name === 'Profile') {
-            iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline'
-
-            return <MaterialIcons name={'account-circle'} size={size} color={color} />
-          } else if (route.name === 'SearchServices') {
-            iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline'
-
-            return <MaterialIcons name={'search'} size={size} color={color} />
-          } else if (route.name === 'ProposalsStack') {
-            iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline'
-
-            return <Entypo name="text-document" size={size} color={color} />
-          }
-          // colocar mais ifs caso tenha mais itens na navbar
-        },
-        tabBarHideOnKeyboard: true,
-        tabBarStyle: {
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'white',
-          height: 60,
-        },
-        tabBarActiveTintColor: '#3030C2',
-        tabBarInactiveTintColor: '#151553',
-        tabBarShowLabel: false,
-      })}
+      screenOptions={{ headerShown: false }}
+      tabBar={NavBar}
     >
-      <Tab.Screen name="ProposalsStack" component={ProposalsStackScreen} />
+      <Tab.Screen name="Proposals" component={ProposalsScreen} />
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Profile" component={Profile} />
       <Tab.Screen name="SearchServices" component={SearchServices} />
     </Tab.Navigator>
   )
-}
-
-const AuthenticatedNavigation = () => {
-  return <></>
 }
 
 export default RootNavigation
--- src/screens/Proposals/Proposals.tsx
+++ src/screens/Proposals/Proposals.tsx
@@ -1,27 +1,14 @@
 import React from 'react'
-import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import PreferencesWarning from '@Components/atomic/PreferencesWarning/PreferencesWarning'
 
-import Preferences from '@Screens/Preferences/Preferences'
-
-const ProposalsStack = createNativeStackNavigator()
-
-const ProposalsStackScreen = () => {
+const ProposalsScreen = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <ProposalsStack.Navigator screenOptions={{ headerShown: false }}>
-        <ProposalsStack.Screen name="Home" component={Proposals} />
-        <ProposalsStack.Screen name="Preferences" component={Preferences} />
-      </ProposalsStack.Navigator>
+      <PreferencesWarning />
     </SafeAreaView>
   )
 }
 
-const Proposals = () => {
-  return <PreferencesWarning />
-}
-
-export default ProposalsStackScreen
+export default ProposalsScreen
