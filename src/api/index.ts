b7fe042a0b88fe1ca0c9650ecf3d6b9d291f2b7b
Filipe Neves <filipe.nevser@gmail.com>
2023-06-04 00:19:00 -0300

    feat: add axios interceptor to use access token

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,22 @@
 import axios, { AxiosInstance } from 'axios'
+import store from '@Store/configureStore'
 
-const HOST = 'http://172.16.233.152'
-export const mainApi: AxiosInstance = axios.create({
-  baseURL: `${HOST}:3333`,
+const HOST = 'http://192.168.0.13'
+
+const mainApi: AxiosInstance = axios.create({
+  baseURL: `${HOST}:3332`,
 })
 
-export const authApi: AxiosInstance = axios.create({
+mainApi.interceptors.request.use((config) => {
+  const { token } = store.getState().auth
+
+  if (token) config.headers.Authorization = `Bearer ${token.value}`
+
+  return config
+})
+
+const authApi: AxiosInstance = axios.create({
   baseURL: `${HOST}:3330`,
 })
+
+export { mainApi, authApi }
