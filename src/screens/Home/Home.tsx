82231576e17b792193be44deaccb0303b96480e9
Filipe Neves <filipe.nevser@gmail.com>
2023-08-20 17:45:38 -0300

    fix: remove home page user logged fetch

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,12 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Text } from 'react-native'
 import styled from 'styled-components/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { useAppDispatch, useAppSelector } from '@Hooks/redux'
-import UserService from '@Api/services/userService'
-import { insertLoggedUserInfo } from '@Store/reducers/user'
-import { insertAuthInfo } from '@Store/reducers/auth'
-import { secureStoreSave } from '@Utils/secureStore'
 
 const Container = styled.View`
   flex: 1;
@@ -22,36 +17,6 @@ const BodyContainer = styled.View`
 `
 
 const Home: React.FC = () => {
-  const dispatch = useAppDispatch()
-  const { token, refreshToken } = useAppSelector(({ auth }) => auth)
-
-  async function loadLoggedUser() {
-    const response = await UserService.getLoggedUser()
-    dispatch(insertLoggedUserInfo(response))
-
-    setInterval(async () => {
-      if (!token || !refreshToken) return
-
-      const refreshResponse = await UserService.refreshToken(
-        token.value,
-        refreshToken.value
-      )
-
-      const newTokenObject = {
-        token,
-        refreshToken,
-        ...refreshResponse,
-      }
-
-      dispatch(insertAuthInfo({ ...newTokenObject, isLogged: true }))
-      secureStoreSave('secureToken', JSON.stringify(newTokenObject))
-    }, 600000)
-  }
-
-  useEffect(() => {
-    loadLoggedUser()
-  }, [])
-
   return (
     <Container>
       <SafeAreaView>
